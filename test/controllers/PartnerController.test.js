45d67eecef2849c450ad7db8b182649633b5cde7
AndreaCuriel <acurielipn@hotmail.com>
2022-04-27 22:20:32 -0500

    25) Prueba fallida en PartnerController  test 3 (getPartnersWithNumCredits)

--- test/controllers/PartnerController.test.js
+++ test/controllers/PartnerController.test.js
@@ -11,4 +11,9 @@ describe("PartnerController Test", () => {
       PartnerController.getPartnersEmailWthiCertification(true);
     expect(virtualpartners[0]).toBe("Todd@visualpartnership.xyz");
   });
+
+  test("3) Regresar partners que tengas creditos mayores a x", () => {
+    const virtualpartners = PartnerController.getPartnersWithNumCredits(500);
+    expect(virtualpartners[0].name).toBe("Warren1");
+  });
 });
