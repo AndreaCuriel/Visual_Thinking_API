18ece868583b098f2fb49986787149be99481c40
AndreaCuriel <acurielipn@hotmail.com>
2022-04-27 22:10:10 -0500

    22) Cambiar nombre del método para llamar todo los partners por getAllPartners en la prueba de PartnerController

--- test/controllers/PartnerController.test.js
+++ test/controllers/PartnerController.test.js
@@ -2,8 +2,13 @@ const PartnerController = require("./../../lib/controllers/PartnerController");
 
 describe("PartnerController Test", () => {
   test("1) Regresar todos los Partners", () => {
-    const virtualpartners =
-      PartnerController.getPartnersEmailWthiCertification();
+    const virtualpartners = PartnerController.getAllPartner();
     expect(virtualpartners[0].name).toBe("Warren");
+  });
+
+  test("2) Regresar emial de los Partners con certificado", () => {
+    const virtualpartners =
+      PartnerController.getPartnersEmailWthiCertification(true);
+    expect(virtualpartners[0]).toBe("Todd@visualpartnership.xyz");
   });
 });
