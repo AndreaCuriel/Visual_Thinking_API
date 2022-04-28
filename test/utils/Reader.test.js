ec24d59b54ba3d03ccf36e84a450e65c7a2b62b8
AndreaCuriel <acurielipn@hotmail.com>
2022-04-27 20:19:33 -0500

    7) Prueba fallida para la clase Reader

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,9 @@
+const Reader = require("./../../lib/utils/Reader");
+
+describe("Reader Test", () => {
+  test("1) Read visualpartners,json", () => {
+    const visualpartners = Reader.readJsonFile("visualpartners.json");
+    const partner = visualpartners[0].name;
+    expect(partner).toBe("Warren1");
+  });
+});
