d676cc23d2bda99705e87df82a94e361d5d80de2
AndreaCuriel <acurielipn@hotmail.com>
2022-04-27 21:57:24 -0500

    20) Prueba exitosa  para la clase PartnerController en test 1(getPartnersEmailWthiCertification)

--- test/controllers/PartnerController.test.js
+++ test/controllers/PartnerController.test.js
@@ -4,6 +4,6 @@ describe("PartnerController Test", () => {
   test("1) Regresar todos los Partners", () => {
     const virtualpartners =
       PartnerController.getPartnersEmailWthiCertification();
-    expect(virtualpartners[0].name).toBe("Warren1");
+    expect(virtualpartners[0].name).toBe("Warren");
   });
 });
