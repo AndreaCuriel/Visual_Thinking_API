74d49f499a6347944231e394c17f982333594ff2
AndreaCuriel <acurielipn@hotmail.com>
2022-04-27 21:53:49 -0500

    19) Prueba fallida para la clase PartnerController en test 1(getPartnersEmailWthiCertification)

---
+++ test/controllers/PartnerController.test.js
@@ -0,0 +1,9 @@
+const PartnerController = require("./../../lib/controllers/PartnerController");
+
+describe("PartnerController Test", () => {
+  test("1) Regresar todos los Partners", () => {
+    const virtualpartners =
+      PartnerController.getPartnersEmailWthiCertification();
+    expect(virtualpartners[0].name).toBe("Warren1");
+  });
+});
